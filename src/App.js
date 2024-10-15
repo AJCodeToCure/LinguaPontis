e381923831642c06369cf7a1258204b5490e8c0b
Talha-Dogar <dogartalha299@gmail.com>
2024-10-15 21:04:33 +0500

    fix-add routes to pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AgencyUser from "./pages/agencyUser/AgencyUser";
 import AgencyManagement from "./pages/agencyManagement/AgencyManagement";
 import DashboardLayout from "./pages/dashboard/Dashboard";
@@ -6,8 +7,8 @@ import Login from "./pages/login/Login";
 import MediatorAvailability from "./pages/mediatorAvailability/MediatorAvailability";
 import UserManagement from "./pages/userManagement/UserManagement";
 import TeamDetails from "./pages/teamDetails/TeamDetails";
-
-
+import TaskManagement from "./pages/taskManagement/TaskManagement";
+import BeneficiaryManagement from "./pages/beneficiaryManagement/BeneficiaryManagement";
 export default function App() {
   return (
   <div>
@@ -18,7 +19,22 @@ export default function App() {
     {/* <EventManagementPage /> */}
     {/* <TeamDetails /> */}
      {/* <MediatorAvailability />  */}
-    <UserManagement />
+    {/* <UserManagement /> */}
+
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/dashboard" element={<DashboardLayout/>} />
+        <Route path="/eventmn" element={<EventManagementPage/>} />
+        <Route path="/mediatorav" element={<MediatorAvailability/>} />
+        <Route path="/usermn" element={<UserManagement/>} />
+        <Route path="/agencymn" element={<AgencyManagement/>} />
+        <Route path="/teamdt" element={<TeamDetails/>} />
+        <Route path="/agencyuser" element={<AgencyUser/>} />
+        <Route path="/benefmn" element={<BeneficiaryManagement/>}/>
+        <Route path="/taskmn" element={<TaskManagement/>}/>
+        </Routes>
+    </BrowserRouter>
   </div>
   )
 }
